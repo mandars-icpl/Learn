825794484b31851f1e927710cc5c758a602f6590
Mandar Sanghavi <138677476+mandars-icpl@users.noreply.github.com>
2024-03-20 14:46:28 +0530

    Created Repo for all learning concepts and projects

---
+++ dotnet-Web-Api-Projects/ConstoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using ConstoPizza.Models;
+using ConstoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstoPizza.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController:Controller
+    {
+        public PizzaController()
+        {
+
+        }
+        //TODO : check Difference between ActionResut and IActionResult
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAllPizzas()=>PizzaServices.GetAllPizzas();
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaServices.GetPizzaById(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+    }
+}
